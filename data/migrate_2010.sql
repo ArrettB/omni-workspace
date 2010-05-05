e8ffb4ab28f9204fb079b379d7eca510316b92f5
Dave Andreasen <dandreasen@kettleriverconsulting.com>
2010-05-05 18:49:57 +0000

    Added lookups entries and details table.

---
+++ data/migrate_2010.sql
@@ -0,0 +1,34 @@
+/**
+ * Add the columns to service_lines for timekeeping
+ * and the column to requests for the plan_location_id
+ */
+/*
+ * Alter the requests table and add the foreign key
+ */
+ALTER TABLE [dbo].[REQUESTS]
+   ADD [PLAN_LOCATION_TYPE_ID]  [NUMERIC](18,0)
+GO
+
+ALTER TABLE [REQUESTS] ADD CONSTRAINT [FK_REQUESTS_LOOKUPS_PLAN_LOCATION_TYPE] 
+    FOREIGN KEY ([PLAN_LOCATION_TYPE_ID]) REFERENCES [LOOKUPS] ([LOOKUP_ID])
+GO
+
+
+/*
+ * Alter the requests table and add the foreign key
+ */
+ALTER TABLE [dbo].[SERVICE_LINES]
+   ADD [START_TIME]  [INTEGER]
+GO
+ALTER TABLE [dbo].[SERVICE_LINES]
+   ADD [END_TIME]  [INTEGER]
+GO
+ALTER TABLE [dbo].[SERVICE_LINES]
+   ADD [BREAK_TIME_MINUTES]  [INTEGER]
+GO
+ALTER TABLE [dbo].[SERVICE_LINES]
+   ADD [END_TIME]  [INTEGER]
+GO
+ALTER TABLE [dbo].[SERVICE_LINES]
+   ADD [HOURS_WORKED] [INTEGER]
+GO
---
+++ data/plan_locations_lookups.sql
@@ -0,0 +1,53 @@
+/*
+ * SQL script to create the HOTSHEET_DETAILS lookup types and LOOKUPS entries
+ *
+ * Created by Dave Andreasen - Kettle River Consulting
+ */
+SET IDENTITY_INSERT LOOKUP_TYPES on
+INSERT
+INTO
+    dbo.LOOKUP_TYPES
+    (
+        LOOKUP_TYPE_ID,
+        CODE,
+        NAME,
+        ACTIVE_FLAG,
+        UPDATABLE_FLAG,
+        PARENT_TYPE_ID,
+        DATE_CREATED,
+        CREATED_BY,
+    )
+    VALUES
+    (
+        84,
+        'plan_locations_type',
+        'Plan Locations',
+        'Y',
+        'Y',
+        0,
+        '',
+        NULL,
+        CURRENT_TIMESTAMP,
+        2
+    )
+GO
+SET IDENTITY_INSERT LOOKUP_TYPES off
+
+/*
+ * LOOKUPS
+ */
+INSERT INTO dbo.LOOKUPS ( LOOKUP_TYPE_ID, CODE, NAME, SEQUENCE_NO, ACTIVE_FLAG, UPDATABLE_FLAG, DATE_CREATED,CREATED_BY) VALUES
+    ( 84, 'location_other', 'Other (please specify in scope of work)', 1, 'Y', 'Y', CURRENT_TIMESTAMP, 2 )
+GO
+INSERT INTO dbo.LOOKUPS ( LOOKUP_TYPE_ID, CODE, NAME, SEQUENCE_NO, ACTIVE_FLAG, UPDATABLE_FLAG, DATE_CREATED,CREATED_BY) VALUES
+    ( 84, 'attached_to_st', 'Attached to ST', 2, 'Y', 'Y', CURRENT_TIMESTAMP, 2 )
+GO
+INSERT INTO dbo.LOOKUPS ( LOOKUP_TYPE_ID, CODE, NAME, SEQUENCE_NO, ACTIVE_FLAG, UPDATABLE_FLAG, DATE_CREATED,CREATED_BY) VALUES
+    ( 84, 'give_to_idm', 'Given to IDM', 3, 'Y', 'Y', CURRENT_TIMESTAMP, 2 )
+GO
+INSERT INTO dbo.LOOKUPS ( LOOKUP_TYPE_ID, CODE, NAME, SEQUENCE_NO, ACTIVE_FLAG, UPDATABLE_FLAG, DATE_CREATED,CREATED_BY) VALUES
+    ( 84, 'sent_via_oss', 'Sent via OSS', 4, 'Y', 'Y', CURRENT_TIMESTAMP, 2 )
+GO
+INSERT INTO dbo.LOOKUPS ( LOOKUP_TYPE_ID, CODE, NAME, SEQUENCE_NO, ACTIVE_FLAG, UPDATABLE_FLAG, DATE_CREATED,CREATED_BY) VALUES
+    ( 84, 'no_drawing', 'No drawing provided', 5, 'Y', 'Y', CURRENT_TIMESTAMP, 2 )
+GO
