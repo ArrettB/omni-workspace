7f5e1db592b1b2a5d6eb6afe416f61e418f46b0e
Paul Garvie <pgarvie@kettleriverconsulting.com>
2016-03-25 15:09:18 +0000

    password conversion

---
+++ servicetrax/stc/src/main/java/com/dynamic/servicetrax/service/EncryptionHelper.java
@@ -0,0 +1,38 @@
+package com.dynamic.servicetrax.service;
+
+import org.apache.commons.codec.digest.DigestUtils;
+
+/**
+ * User: pgarvie
+ * Date: 3/21/16
+ * Time: 9:22 AM
+ */
+public class EncryptionHelper {
+
+    private static final EncryptionHelper INSTANCE = new EncryptionHelper();
+
+    private String[] SALTS = {
+            "BXCob9QnW0B64/zragEP9g==",
+            "ROVRbFnrVz6VoCnsH+S1RQ==",
+            "0UAW/1tU46EUiLqgYEtn0Q==",
+            "4HlALlJIkzzGOG2ZSXAX7g==",
+            "fT+TLctUl2s+mRn1V03dLg==",
+            "syP5fy4dUQHPD7jZeC/oGQ==",
+            "hvYN+zIiiPqi/lFVQWqmrQ==",
+            "t/cd2UuMhUd/LqK1/Q6vEQ==",
+            "hLDxEMEirJfm4wXXf8mWNQ==",
+            "zK5Tye7XNHZ47kLvaCF67w=="
+    };
+
+    public String hash(String username, String password) {
+        String salt = SALTS[Math.abs(username.hashCode() % 10)];
+        return DigestUtils.sha256Hex(salt + password);
+    }
+
+    public static EncryptionHelper getInstance() {
+        return INSTANCE;
+    }
+
+    private EncryptionHelper() {
+    }
+}
